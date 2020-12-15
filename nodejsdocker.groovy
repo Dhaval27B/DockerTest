e82fa0b5d60a4a5267c2f551d5fc32f1cbeecfcf
Dhaval27B <75625565+Dhaval27B@users.noreply.github.com>
2020-12-15 07:08:06 -0800

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('dhaval27/docker-nodejs-demo')
+            repositoryName('dhavalb27/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
